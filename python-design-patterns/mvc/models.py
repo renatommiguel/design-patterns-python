226094abb44b77bc682eb2c521cf509d6766790f
G14-ROG-LAPTOP\renat <renatommiguel@gmail.com>
2022-11-19 00:54:36 +0100

    commit

---
+++ python-design-patterns/mvc/models.py
@@ -0,0 +1,11 @@
+
+import pickle
+
+class Url():
+
+    @classmethod
+    def shorten(cls, full_url):
+        """"shroten full path"""
+        instance = cls()
+        instance = cls()
+        instance
